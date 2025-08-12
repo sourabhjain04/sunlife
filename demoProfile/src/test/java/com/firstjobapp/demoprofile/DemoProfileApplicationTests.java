91e88c587bfe98977b9d7eaaac34d620b69aa4f4
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-08-12 09:56:10 +0530

    Add files via upload

---
+++ demoProfile/src/test/java/com/firstjobapp/demoprofile/DemoProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstjobapp.demoprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
